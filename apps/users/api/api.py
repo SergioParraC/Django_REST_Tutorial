9ee8fa2981e0ef1875dbc3dfcced05df6e36cc50
Sergio Parra <stevenparracuesta@gmail.com>
2023-09-23 23:32:40 -0500

    Clase 4 Decorador @API_VIEW

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,14 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
-class UserAPIView(APIView):
-
-    def get(self, request):
+#Ahora se va a utilizar un decorador en vez de una clase, en el decorador se le tiene que añadir
+#Los metodos que se van a ingresar, en este caso son GET y POST
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    #Se cambia la estructura de clase a funcion, se tiene que comporbar el tipo de metodo enviado para
+    #las difetentes acciones que debe realizar
+    if request.method =='GET':
         #Extrae la información que se requiere
         users = User.objects.all()
         #Se hace el tratamiento de la información, se pone many=True porque va a traer muchos archivos
         user_serializer = UserSerializer(users, many= True)
         #Se retorna con response a la vista, PERO la informacion se encuentra en el atributo .data
         return Response(user_serializer.data)
+    elif request.method == 'POST':
+        print(request.data)
--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import user_api_view
 
 urlpatterns = [
     #Se crea el nuevo enlace, se llama a la vista
-    path('usuario/', UserAPIView.as_view(), name = 'usuario_api')
+    path('usuario/', user_api_view, name = 'usuario_api')
 ]
