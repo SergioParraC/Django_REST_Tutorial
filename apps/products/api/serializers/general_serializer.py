3527e025ccb63f92c740ceae720e2e375884c3b6
Sergio Parra <stevenparracuesta@gmail.com>
2023-09-28 19:03:53 -0500

    Clase 16 - Serializadores de Aplicación Productos

---
+++ apps/products/api/serializers/general_serializer.py
@@ -0,0 +1,20 @@
+from apps.products.models import MeasureUnit, CategoryProduct, Indicator
+from rest_framework import serializers
+
+class MeasuereUnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MeasureUnit
+        #De todos los campos que tiene, este no lo va a retornar en la consulta
+        exclude = ('state', )
+
+class CategoryProductsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CategoryProduct
+        exclude = ('estate', )
+
+class IndicadorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Indicator
+        exclude = ('state', )
